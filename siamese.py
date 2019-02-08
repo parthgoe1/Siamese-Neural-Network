7a1c823d8de362a6f2f19cd97f5cdd6539ddaaf4
Parth Goel <parth.179301138@muj.manipal.edu>
2019-02-09 02:57:56 +0530

    dataloader done

--- siamese.py
+++ siamese.py
@@ -57,3 +57,89 @@ for i in range(0,360,1):
     if(k == 0):
         k = 10
         
+class SiameseNetworkDataset(Dataset):
+    
+    def __init__(self,genuine_pairs,imposter_pairs,transform=None,should_invert=True):
+        self.genuine_pairs = genuine_pairs
+        self.imposter_pairs = imposter_pairs
+        self.transform = transform
+        self.should_invert = should_invert        
+        
+    def __getitem__(self,index):
+        #choosing randomly betweeen genuine(0) and imposter(1) pair
+        choice = random.randint(0,1)
+        
+        #using if else to choose a random pair
+        if(choice == 0):
+            #genuine pair(0)
+            
+            #select a genuine pair
+            pair = random.choice(self.genuine_pairs)
+            
+            #image 1
+            img0 = pair[0]
+            
+            #image 2
+            img1 = pair[1]
+            
+            #genuine label
+            label = pair[2]
+            
+        else:
+            #imposter pair(1)
+            
+            #select an imposter pair
+            pair = random.choice(self.imposter_pairs)
+            
+            #image 1
+            img0 = pair[0]
+            
+            #image 2
+            img1 = pair[1]
+            
+            #genuine label
+            label = pair[2]
+            
+        #open the images
+        img0 = Image.open(img0)
+        img1 = Image.open(img1)
+        
+        #converting the images to black and white
+        img0 = img0.convert("L")
+        img1 = img1.convert("L")
+ 
+        if self.should_invert:
+            img0 = PIL.ImageOps.invert(img0)
+            img1 = PIL.ImageOps.invert(img1)
+
+        if self.transform is not None:
+            img0 = self.transform(img0)
+            img1 = self.transform(img1)
+            
+        return img0, img1, torch.from_numpy(np.array([int(label)], dtype=np.float32))
+    
+    def __len__(self):
+        return len(self.genuine_pairs) + len(self.imposter_pairs)
+
+
+siamese_dataset = SiameseNetworkDataset(genuine_pairs = genuine_pairs, imposter_pairs = imposter_pairs,
+                                        transform=transforms.Compose([transforms.Resize((105,105)),
+                                                                      transforms.ToTensor()])                                                                      
+                                       ,should_invert=False)
+    
+vis_dataloader = DataLoader(siamese_dataset,
+                        shuffle=True,
+                        #num_workers=8,
+                        batch_size=8)
+
+dataiter = iter(vis_dataloader)
+
+
+example_batch = next(dataiter)
+concatenated = torch.cat((example_batch[0],example_batch[1]),0)
+imshow(torchvision.utils.make_grid(concatenated))
+print(example_batch[2].numpy())
+
+
+
+        
