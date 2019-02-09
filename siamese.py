b8e1968ae59a9302c83d42e005d8e3af9d0cccd1
Parth Goel <parth.179301138@muj.manipal.edu>
2019-02-09 14:45:53 +0530

    normalization added added

--- siamese.py
+++ siamese.py
@@ -151,6 +151,9 @@ class SiameseNetwork(nn.Module):
         self.cnn1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=10, stride=1, padding=0)
         self.relu1 = nn.ReLU()
         
+        # Normalization 1
+        self.norm1 = nn.LocalResponseNorm(5,alpha=0.0001,beta=0.75,k=2)
+        
         # Max Pooling 1
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         
@@ -158,8 +161,14 @@ class SiameseNetwork(nn.Module):
         self.cnn2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=7, stride=1, padding=0)
         self.relu2 = nn.ReLU()
         
+        # Normalization 2
+        self.norm2 = nn.LocalResponseNorm(5,alpha=0.0001,beta=0.75,k=2)
+        
         # Max Pooling 2
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        
+        # Dropout 1
+        self.drop1 = nn.Dropout2d(p=0.3)
         
         # Convolution 3
         self.cnn3 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=4, stride=1, padding=0)
@@ -172,9 +181,15 @@ class SiameseNetwork(nn.Module):
         self.cnn4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=1, padding=0)
         self.relu4 = nn.ReLU()
         
+        # Dropout 2
+        self.drop2 = nn.Dropout2d(p=0.3)
+        
         # Fully Connected 1
         self.fc1 = nn.Linear(256 * 6 * 6, 4096)
         self.sig1 = nn.Sigmoid()
+        
+        # Dropout 3
+        self.drop3 = nn.Dropout2d(p=0.5)
         
         # Fully Connected 2
         self.fc2 = nn.Linear(4096, 1)
@@ -186,6 +201,9 @@ class SiameseNetwork(nn.Module):
         out = self.cnn1(x)
         out = self.relu1(out)
         
+        # Normalization 1
+        out =  self.norm1(out)
+        
         # Max pool 1
         out = self.pool1(out)
         
@@ -193,8 +211,14 @@ class SiameseNetwork(nn.Module):
         out = self.cnn2(out)
         out = self.relu2(out)
         
+        # Normalization 2
+        out =  self.norm2(out)
+        
         # Max pool 2
         out = self.pool2(out)
+        
+        # dropout 1
+        out = self.drop1(out)
         
         # Convolution 3
         out = self.cnn3(out)
@@ -207,12 +231,18 @@ class SiameseNetwork(nn.Module):
         out = self.cnn4(out)
         out = self.relu4(out)
         
+        # dropout 2
+        out = self.drop2(out)
+        
         # Resize
         out = out.view(out.size(0), -1)
         
         # Fully Connected 1
         out = self.fc1(out)
         out = self.sig1(out)
+        
+        # dropout 3
+        out = self.drop3(out)
         
         # Resize
         out = out.view(out.size(0), -1)
