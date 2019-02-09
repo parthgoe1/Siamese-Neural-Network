05333e8c65a997c5ef01d45e56f404e9fc8211bd
Parth Goel <parth.179301138@muj.manipal.edu>
2019-02-09 14:28:19 +0530

    weights initialization added

--- siamese.py
+++ siamese.py
@@ -251,4 +251,19 @@ class ContrastiveLoss(torch.nn.Module):
 
 
         return loss_contrastive
+    
+#weights initializiation
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.01)
+        m.bias.data.normal_(0.5, 0.01)
+# =============================================================================
+#     elif classname.find('BatchNorm') != -1:
+#         m.weight.data.normal_(1.0, 0.02)
+#         m.bias.data.fill_(0)    
+# =============================================================================
+    elif classname.find('Linear') != -1:
+        m.weight.data.normal_(0.0, 0.2)
+        m.bias.data.normal_(0.5, 0.01) 
         
